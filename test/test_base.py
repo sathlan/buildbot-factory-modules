2d8cf4154aa17337331fe25fa0c1d3d359d9b88d
sathlan <mypublicaddress-code@ymail.com>
2012-05-14 09:58:37 +0200

    Fix test_base.

--- test/test_base.py
+++ test/test_base.py
@@ -5,19 +5,26 @@ from ferbot.error import MyCommandError
 
 ''' Test the base class '''
 
-class CaseTest(TestCase):
-    dummy    = Dummy(directory='test',vms=[])
+class BaseTest(TestCase):
+    def setUp(self):
+        self.factory  = []
+        self.dummy = Dummy(directory='test',vms=[], force_new_builders = True)
 
-    def test_compile(self):
+    def test_compile_base(self):
         self.assertEquals("test@name", self.dummy.name)
         self.assertEquals(False, self.dummy.is_vm)
 
-    def test_commands(self):
+    def test_commands_base(self):
         self.assertRaises(MyCommandError, self.dummy.commands.simple)
         
-    def test_start(self):
+    def test_start_base(self):
         self.builders = self.dummy.start()
-        self.assertEquals(set([self.dummy]),self.builders.roots())
+        self.assertEquals(set([self.dummy]), self.builders.roots())
+        self.assertEquals(1, len(self.builders.roots()))
         self.assertEquals(['bash', '-c', 'source test/dummy ; onetest'],
                           self.dummy.commands.simple(['onetest']))
-        
+        self.assertItemsEqual(['test@name', 'test@name_quick'],
+                          self.builders.get_builders_name())
+        self.assertEquals({'nbr_of_elements': 2, 'nbr_of_steps': 1, 'nbr_of_chains': 2},
+                          self.dummy.stats)
+
